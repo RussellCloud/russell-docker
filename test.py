67657220d1ed9496941ec5ecbae35f6bca5e1871
zhourunlai <xiaorun95@gmail.com>
2017-03-21 12:16:51 +0800

    update test

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ client = docker.from_env()
 
 print client.containers.list()
 
-container = client.containers.run("alpine", ["echo", "hello", "world"])
+container = client.containers.run("alpine", ["echo", "hello", "world"], detach=True)
 
 for line in container.logs(stream=True):
     print line.strip()
