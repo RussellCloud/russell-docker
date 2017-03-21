ac263a8094ed8fafca88f0dc32d1100e7879f464
zhourunlai <xiaorun95@gmail.com>
2017-03-21 12:23:03 +0800

    update test

--- test.py
+++ test.py
@@ -2,15 +2,19 @@
 
 import docker
 
+# 创建实例
 client = docker.from_env()
+# client = docker.DockerClient(base_url='unix://var/run/docker.sock')
+# client = docker.DockerClient(base_url='tcp://127.0.0.1:1234')
 
+# 容器列表
 print client.containers.list()
 
+# 运行容器
 container = client.containers.run("alpine", ["echo", "hello", "world"], detach=True)
 
 print container.id
 print container.attrs['Config']['Image']
-
 for line in container.logs(stream=True):
     print line.strip()
 
