cae431b763d36ece6c737c1c389f23958a270948
zhourunlai <xiaorun95@gmail.com>
2017-03-21 12:12:53 +0800

    update test

--- test.py
+++ test.py
@@ -1,4 +1,17 @@
+# -*- coding: utf-8 -*-
+
 import docker
 
 client = docker.from_env()
-print client.containers.run("alpine", ["echo", "hello", "world"])
+
+print client.containers.list()
+
+container = client.containers.run("alpine", ["echo", "hello", "world"])
+
+print container.id
+print container.attrs['Config']['Image']
+
+for line in container.logs(stream=True):
+    print line.strip()
+
+container.stop()
