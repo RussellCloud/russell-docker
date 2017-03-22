3e3b58dbb3cd47eb6c0e9ca8448ad21c0eed8760
zhourunlai <xiaorun95@gmail.com>
2017-03-22 23:18:09 +0800

    update python package

--- test_service.py
+++ test_service.py
@@ -1,10 +1,8 @@
+import os
 from Russelldocker import Service
 
 myService = Service()
 
 print myService.list()
 
-print myService.run('floydhub/tensorflow:latest-py2',
-                    'floydhub',
-                    'replicated',
-                    ['-p 8888:8888'])
+os.system('docker service create --name floydhub --publish 8888:8888 floydhub/tensorflow:latest-py2')
