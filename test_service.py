8b27157b582af0a833ab6bc51ba8895435677803
zhourunlai <xiaorun95@gmail.com>
2017-03-24 00:58:23 +0800

    add volume

--- test_service.py
+++ test_service.py
@@ -7,7 +7,7 @@ myService = Service()
 # service = myService.get('h2mo8de6fj')
 service = myService.create(image='floydhub/tensorflow:latest-py2',
                            name='floydhub',
-                           source='~/tensorflow-examples/3_NeuralNetworks',
+                           source='/root/tensorflow-examples/3_NeuralNetworks',
                            target='~/code',
                            command=['cd ~/code', 'jupyter notebook'],
                            run_mode='jupyter')
