164b36490eda91935521380e50d7f6c61ddcf3f6
zhourunlai <xiaorun95@gmail.com>
2017-03-25 01:05:38 +0800

    update python test

--- test_service.py
+++ test_service.py
@@ -5,14 +5,14 @@ myService = Service()
 # print myService.list()
 
 # service = myService.get('h2mo8de6fj')
-# service = myService.create(image='floydhub/tensorflow:latest-py2',
-#                            name='floydhub',
-#                            source='/root/tensorflow-examples/3_NeuralNetworks',
-#                            target='/code',
-#                            constraints=['node.hostname==russell-master'],
-#                            command='',
-#                            workdir='/code',
-#                            run_mode='jupyter')
+service = myService.create(image='floydhub/tensorflow:latest-py2',
+                           name='floydhub',
+                           source='/root/tensorflow-examples/3_NeuralNetworks',
+                           target='/code',
+                           constraints=['node.hostname==russell-master'],
+                           command='',
+                           workdir='/code',
+                           run_mode='jupyter')
 # service = myService.create(image='floydhub/tensorflow:latest-py2',
 #                            name='floydhub',
 #                            source='/root/tensorflow-examples/3_NeuralNetworks',
@@ -22,4 +22,4 @@ myService = Service()
 
 # print myService.get_stats('m3o22enlncxk6cs8kjkzt6j7b')
 
-print myService.get_logs('m3o22enlncxk6cs8kjkzt6j7b')
+# print myService.get_logs('m3o22enlncxk6cs8kjkzt6j7b')
