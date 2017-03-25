b3f2c06ffb303867a8366b2755e65050cc2c1700
zhourunlai <xiaorun95@gmail.com>
2017-03-25 15:36:06 +0800

    update python test

--- test_service.py
+++ test_service.py
@@ -21,11 +21,11 @@ myService = Service()
 #                            command='python /code/dynamic_rnn.py',
 #                            run_mode='cli')
 service = myService.create(image='floydhub/tensorflow:latest-py3',
-                           name='w821881341/rnn:52',
+                           name='w821881341',
                            source='/root/code/9bef47551eb3422d907d324387c13ee6',
                            target='/code',
-                           command='python /code/dynamic_rnn.py',
-                           # workdir='',
+                           command='python dynamic_rnn.py',
+                           workdir='/code',
                            run_mode='cli')
 
 # print myService.get_stats('kbtvses1f6ucok7j16v0mzo86')
