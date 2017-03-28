493bb68dd72b3e57d0919c432b5a5711f7c54da0
zhourunlai <xiaorun95@gmail.com>
2017-03-28 11:35:25 +0800

    update python test

--- test_service.py
+++ test_service.py
@@ -20,18 +20,18 @@ myService = Service()
 #                            target='/code',
 #                            command='python /code/dynamic_rnn.py',
 #                            run_mode='cli')
-# id = myService.create(image='floydhub/tensorflow:latest-py3',
-#                       name='w821881341_rnn_test',
-#                       source='/root/code/9bef47551eb3422d907d324387c13ee6',
-#                       target='/code',
-#                       command='python dynamic_rnn.py',
-#                       workdir='/code',
-#                       run_mode='cli')
+id = myService.create(image='floydhub/tensorflow:latest-py3',
+                      name='w821881341_rnn_test',
+                      source='/root/code/9bef47551eb3422d907d324387c13ee6',
+                      target='/code',
+                      command='python dynamic_rnn.py',
+                      workdir='/code',
+                      run_mode='cli')
 
-# print myService.get_stats(id)
+print myService.get_stats(id)
 
-log_gen = myService.get_logs('uh323v6dos4i')
+# log_gen = myService.get_logs('uh323v6dos4i')
 # print log_gen.next()
-log_list = list(log_gen)
-for log in log_list:
-    print log
+# log_list = list(log_gen)
+# for log in log_list:
+#     print log
