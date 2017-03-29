c214a0fc2a2cb2619e5635347455857e9c3ebec1
zhourunlai <xiaorun95@gmail.com>
2017-03-29 23:49:06 +0800

    update python test

--- test_service.py
+++ test_service.py
@@ -8,27 +8,21 @@ myService = Service()
 
 # service = myService.create(image='floydhub/tensorflow:latest-py2',
 #                            name='floydhub',
-#                            source='/root/tensorflow-examples/3_NeuralNetworks',
+#                            source='/root/code/9bef47551eb3422d907d324387c13ee6',
 #                            target='/code',
 #                            constraints=['node.hostname==russell-master'],
 #                            command='',
 #                            workdir='/code',
 #                            run_mode='jupyter')
-# service = myService.create(image='floydhub/tensorflow:latest-py2',
-#                            name='floydhub',
-#                            source='/root/tensorflow-examples/3_NeuralNetworks',
-#                            target='/code',
-#                            command='python /code/dynamic_rnn.py',
-#                            run_mode='cli')
-id = myService.create(image='floydhub/tensorflow:latest-py3',
-                      name='w821881341_rnn_test',
-                      source='/root/code/9bef47551eb3422d907d324387c13ee6',
-                      target='/code',
-                      command='python dynamic_rnn.py',
-                      workdir='/code',
-                      run_mode='cli')
-
-print myService.get_stats(id)
+# id = myService.create(image='floydhub/tensorflow:latest-py3',
+#                       name='w821881341_rnn_test',
+#                       source='/root/code/9bef47551eb3422d907d324387c13ee6',
+#                       target='/code',
+#                       command='python dynamic_rnn.py',
+#                       workdir='/code',
+#                       run_mode='cli')
+#
+# print myService.get_stats(id)
 
 # log_gen = myService.get_logs('uh323v6dos4i')
 # print log_gen.next()
@@ -37,4 +31,11 @@ print myService.get_stats(id)
 #     print log
 
 # oos
+# sudo apt-get update
+# sudo apt-get install gdebi-core
+# wget http://docs-aliyun.cn-hangzhou.oss.aliyun-inc.com/assets/attach/32196/cn_zh/1483608175067/ossfs_1.80.0_ubuntu16.04_amd64.deb
+# sudo gdebi ossfs_1.80.0_ubuntu16.04_amd64.deb
+# echo russellcloud:<id>:<sercet> > /etc/passwd-ossfs
+# chmod 640 /etc/passwd-ossfs
+# ossfs russellcloud /root/code -ourl=oss-cn-zhangjiakou-internal.aliyuncs.com
 
